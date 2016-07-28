207fe309b9c2070b22824f4671cee821af139925
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-07-28 11:31:35 +0900

    Use seq_along() instead of seq(along=)

--- R/cfunction.R
+++ R/cfunction.R
@@ -220,14 +220,14 @@ cfunction <- function(sig=character(), body=character(), includes=character(), o
     ## create .C/.Call function call that will be added to 'fn'
     if (convention == ".Call") {
       body <- quote( CONVENTION("EXTERNALNAME", ARG) )[ c(1:2, rep(3, length(sig[[i]]))) ]
-      for ( j in seq(along = sig[[i]]) ) body[[j+2]] <- as.name(names(sig[[i]])[j])
+      for ( j in seq_along(sig[[i]]) ) body[[j+2]] <- as.name(names(sig[[i]])[j])
     }
     else {
       body <- quote( CONVENTION("EXTERNALNAME", as.logical(ARG), as.integer(ARG),
                     as.double(ARG), as.complex(ARG), as.character(ARG),
           			    as.raw(ARG), as.double(ARG)) )[ c(1:2,types[[i]]+2) ]
       names(body) <- c( NA, "", names(sig[[i]]) )
-      for ( j in seq(along = sig[[i]]) ) body[[j+2]][[2]] <- as.name(names(sig[[i]])[j])
+      for ( j in seq_along(sig[[i]]) ) body[[j+2]][[2]] <- as.name(names(sig[[i]])[j])
 ## OLD VERSION -- does not work for lists of functions
 #      body <- quote( CONVENTION("EXTERNALNAME", as.logical(ARG), as.integer(ARG),
 #                    as.double(ARG), as.complex(ARG), as.character(ARG),
